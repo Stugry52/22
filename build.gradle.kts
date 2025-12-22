97a6488cf36d38685590f94ef5875e77411a9228
Stugry52 <vladkravcenko95@gmail.com>
2025-12-22 18:20:25 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,10 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    // dependencies - блок зависимостей (подключение библиотек)
+    // implementation - команда градлу - добавить данную библиотеку в проект для использования
+    // org.jetbrains.kotlinx... - это внутренняя библиотека IDE jb где org -> jetbrains -> kotlinx это вложенные packages
+
 }
 
 tasks.test {
